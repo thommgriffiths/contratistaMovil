4010422b38a376f35ba2d186abe810799fa6e346
Thomas Griffiths <tgriffiths@baufest.com>
2023-01-14 17:50:23 -0300

    feature / reporte rapido contratistas pulido

--- src/pantallasWebAdmin/ReporteContratistas/ReporteRapido.jsx
+++ src/pantallasWebAdmin/ReporteContratistas/ReporteRapido.jsx
@@ -17,21 +17,23 @@ import LoadingComponent from "../../sharedComponents/LoadingComponent";
 import styles from "../styles/Consultar.style";
 import { palette } from "../../Core/colors";
 
+import { renderCapsule } from "./renderCapsule";
 import { AdminReporteRapido as mockData } from "../../Core/util/mockData";
 
 const AdminReporteRapido = () => {
-  const [jornales, setJornales] = useState({});
+  const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
+
   const [filter, setFilter] = useState([
     entities.obra,
     commonAttrs.creadoPor,
     entities.rubro,
   ]);
-  const [data, setData] = useState([]);
 
   useEffect(() => {
     /*
     const loadItems = async () => {
+     
       let queryObject = {
         [commonAttrs.fechaCreacionRango]: getCurrentWeekDates(),
       };
@@ -43,6 +45,8 @@ const AdminReporteRapido = () => {
       const rawElements = await queryFSElements(entities.jornal, query);
 
       const completedElements = await completeElements(rawElements);
+
+      
 
       const datos = consolidarJornales(
         completedElements,
@@ -56,19 +60,16 @@ const AdminReporteRapido = () => {
       console.log(completedElements);
       setJornales(completedElements);
       setLoading(false);
-    };*/
-    //loading ? loadItems() : {};
+    };
+    loading ? loadItems() : {};*/
+
     if (loading) {
       const completedElements = mockData.jornalesSemana;
-      //setJornales(completedElements);
-
-      //cambiar a jornales despues
       const datos = consolidarJornales(
         completedElements,
         ...filter,
         commonAttrs.diasHombre
       );
-
       setData(objectToArray(datos));
       setLoading(false);
     }
@@ -100,28 +101,6 @@ const AdminReporteRapido = () => {
 
 export default AdminReporteRapido;
 
-const renderCapsule = ({ item }) => {
-  var totalNumber = sumInternalValues(item);
-  var titulo = Object.keys(item)[0];
-  return (
-    <View style={customStyles.capsule}>
-      <View style={customStyles.capsuleBody}>
-        <View style={customStyles.capsuleTitle}>
-          <Text style={customStyles.capsuleTitleText}>{titulo}</Text>
-          <View style={customStyles.underline}></View>
-        </View>
-        <View style={customStyles.capsuleData}>
-          <Text>Aca iria la data</Text>
-        </View>
-      </View>
-      <View style={customStyles.capsuleNumber}>
-        <Text style={customStyles.capsuleNumberText}>TOTAL</Text>
-        <Text style={customStyles.capsuleNumberText}>{totalNumber}</Text>
-      </View>
-    </View>
-  );
-};
-
 const consolidarJornales = (objects, first, second, third, value) => {
   var result = {};
 
@@ -147,49 +126,3 @@ const consolidarJornales = (objects, first, second, third, value) => {
 
   return result;
 };
-
-const customStyles = StyleSheet.create({
-  capsule: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    padding: 15,
-    backgroundColor: palette.neutral,
-    marginVertical: 10,
-    borderRadius: 10,
-    borderColor: palette.B1,
-    borderWidth: 2,
-  },
-  capsuleBody: {
-    flex: 1,
-    //padding: 10,
-  },
-  capsuleNumber: {
-    //flex: 2,
-
-    padding: 10,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  capsuleTitle: {
-    flex: 3,
-  },
-  capsuleTitleText: {
-    fontWeight: "bold",
-    color: "black",
-    fontSize: 25,
-    flex: 10,
-  },
-  underline: {
-    backgroundColor: "black",
-    flex: 2,
-  },
-  capsuleData: {
-    flex: 7,
-    paddingTop: 10,
-    alignContent: "center",
-  },
-  capsuleNumberText: {
-    fontWeight: "bold",
-    fontSize: 35,
-  },
-});
