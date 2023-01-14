68271265dce20e77c828e9ed9b5052d6d9026897
Thomas Griffiths <tgriffiths@baufest.com>
2023-01-14 18:19:49 -0300

    refactor / limpieza codigo comentado app.js

--- App.js
+++ App.js
@@ -7,22 +7,6 @@ import { registerTranslation, enGB } from "react-native-paper-dates";
 import LoginScreen from "./src/pantallas/LoginScreen";
 import HomeScreen from "./src/pantallas/HomeScreen";
 
-/*
-//Componentes principales
-import CrearPedidoDeObra from "./src/pantallas/PedidoDeObra/CrearPedidoDeObra";
-import ConsultarPedidosDeObra from "./src/pantallas/PedidoDeObra/ConsultarPedidosDeObra";
-import ConsultarJornales from "./src/pantallas/Jornal/ConsultarJornales";
-import CrearJornal from "./src/pantallas/Jornal/CrearJornal";
-import ConsultarPedidosDeReintegro from "./src/pantallas/PedidoDeReintegro/ConsultarPedidosDeReintegro";
-import CrearPedidoDeReintegro from "./src/pantallas/PedidoDeReintegro/CrearPedidoDeReintegro";
-
-//Componentes ABM datos maestros
-import ConsultarObras from "./src/pantallas/Obra/ConsultarObras";
-import CrearObra from "./src/pantallas/Obra/CrearObra";
-import ConsultarRubros from "./src/pantallas/Rubro/ConsultarRubros";
-import CrearRubro from "./src/pantallas/Rubro/CrearRubro";
-*/
-
 //Pantallas administrador
 import AdminHome from "./src/pantallasWebAdmin/HomeAdmin";
 import AdminEstadosObra from "./src/pantallasWebAdmin/EstadosObra/EstadosObra";
